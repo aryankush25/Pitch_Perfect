d263560dfb0f8685e098c4a79dca733300967f4f
Aryan Agarwal <aryankush025@gmail.com>
2019-10-27 17:37:34 +0530

    Buttons Added For Different Type Of Audios & Audio Extension Added

---
+++ Pitch_Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch_Perfect
+//
+//  Created by Aryan Agarwal on 27/10/19.
+//  Copyright © 2019 Aryan Agarwal. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+
+    enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+    
+
+}
